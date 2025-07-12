5ef1119dfa9c41a6df18af3f24ad28a35747281c
Anna Kafka <anna.kafka933@gmail.com>
2025-07-12 15:01:51 +0200

    Setup production build and static server using build folder

--- server.mjs
+++ server.mjs
@@ -1,14 +1,19 @@
 import jsonServer from 'json-server';
+
 const server = jsonServer.create();
-const router = jsonServer.router('public/db/app.json');
+const router = jsonServer.router('build/db/app.json');
 const middlewares = jsonServer.defaults({
   static: 'build',
   noCors: true
 });
 const port = process.env.PORT || 3131;
+
 server.use(middlewares);
 server.use(jsonServer.rewriter({
   '/api/*': '/$1'
 }));
 server.use(router);
-server.listen(port);
+server.listen(port, () => {
+  console.log(`JSON Server is running at http://localhost:${port}`);
+});
+
